1bc202752d0ae68b94ad5ba460202f1428bce488
mathanmithun1999 <mathanmithun8838@gmail.com>
2023-12-02 17:01:23 +0530

    This is an my first commit for Android App

---
+++ MyBlogger/app/src/main/java/com/example/myblogger/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.myblogger;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+// ... (your other imports)
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://mathanmithun.blogspot.com/?m=0";
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!CheckNetwork.isInternetAvailable(this)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("No internet connection available")
+                    .setMessage("Please Check your Mobile data or Wifi network.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        } else {
+            webview = findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+            webview.loadUrl(websiteURL);
+            webview.setWebViewClient(new WebViewClientDemo());
+        }
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.isFocused() && webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("EXIT")
+                    .setMessage("Are you sure you want to close this app?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
+    }
+}
+
+class CheckNetwork {
+    private static final String TAG = CheckNetwork.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context) {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null) {
+            Log.d(TAG, "no internet connection");
+            return false;
+        } else {
+            return info.isConnected();
+        }
+    }
+}
